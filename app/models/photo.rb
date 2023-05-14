c4279d733b78d09287edf63fc999ce9c08bd39b0
Haidamac <serge.krinitsa@gmail.com>
2023-05-14 21:44:13 +0300

    add active storage

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,7 @@
 class Photo < ApplicationRecord
+  belongs_to :gallery
+  has_one_attached :image do |attachable|
+    attachable.variant :thumb, resize_to_limit: [120, 80]
+    attachable.variant :main, resize_to_limit: [1200, 800]
+  end
 end
---
+++ db/migrate/20230514184119_add_gallery_to_photos.rb
@@ -0,0 +1,5 @@
+class AddGalleryToPhotos < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :photos, :gallery, null: false, foreign_key: true
+  end
+end

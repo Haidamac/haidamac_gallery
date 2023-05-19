c160a849e4b94d1496a8896565d6da5b9490964c
Haidamac <serge.krinitsa@gmail.com>
2023-05-19 22:31:13 +0300

    add amazon s3 images upload

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,9 @@
 require "active_support/core_ext/integer/time"
 
+# Rails.application.routes.default_url_options = {
+#   host: 'https://myhost'
+# }
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -38,7 +42,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon_pro
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 

4cabed7691cc49bbe709f77a13f2e118f921ddbf
Haidamac <serge.krinitsa@gmail.com>
2023-05-27 22:21:20 +0300

    change bootstrap grid for views

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ApplicationRecord
   belongs_to :gallery
   has_one_attached :image do |attachable|
-    attachable.variant :thumb, resize_to_limit: [120, 80]
+    attachable.variant :thumb, resize_to_limit: [240, 160]
     attachable.variant :main, resize_to_limit: [1200, 800]
   end
 end

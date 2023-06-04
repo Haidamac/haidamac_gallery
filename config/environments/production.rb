87322c323a60bc5b096141daf4879f295cdbe081
Haidamac <serge.krinitsa@gmail.com>
2023-06-04 22:01:23 +0300

    change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 require "active_support/core_ext/integer/time"
 
-# Rails.application.routes.default_url_options = {
-#   host: 'https://myhost'
-# }
+Rails.application.routes.default_url_options = {
+  host: 'http://46.101.126.91'
+}
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.

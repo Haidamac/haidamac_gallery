56d16077373d1ca5690faee7a4760c21e906bec5
Haidamac <serge.krinitsa@gmail.com>
2023-06-04 21:40:11 +0300

    change deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,12 +24,12 @@ set :rvm_ruby_version, '3.2.2'
 # Default value for :pty is false
 set :pty, true
 
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/master.key', 'config/puma.rb', 'config/credentials.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/master.key', 'config/puma.rb', 'config/credentials.production.yml')
 set :linked_dirs,  fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'public/images',
                                                 'storage')
 
 set :config_example_suffix, '.example'
-set :config_files, %w[config/database.yml config/credentials.yml]
+set :config_files, %w[config/database.yml config/credentials.production.yml]
 set :nginx_use_ssl, false
 set :env_file, '.env.production.local'
 

db3ede5c5c1e9966aa78964c566a1d9ad2142196
Haidamac <serge.krinitsa@gmail.com>
2023-06-05 22:34:30 +0300

    add production key

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,8 @@ set :rvm_ruby_version, '3.2.2'
 # Default value for :pty is false
 set :pty, true
 
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/master.key', 'config/puma.rb', 'config/credentials.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/master.key', 'config/puma.rb', 
+                                                 'config/credentials.yml', 'config/credentials/production.key')
 set :linked_dirs,  fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'public/images',
                                                 'storage')
 

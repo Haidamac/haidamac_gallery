03d02efeb8406967fbd1ce85d1a3cccaf806dd8c
Haidamac <serge.krinitsa@gmail.com>
2023-06-10 23:34:07 +0300

    change credentials for prod env

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :rvm_ruby_version, '3.2.2'
 set :pty, true
 
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/master.key', 'config/puma.rb', 
-                                                 'config/credentials.yml', 'config/credentials/production.key')
+                                                 'config/credentials.yml')
 set :linked_dirs,  fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'public/images',
                                                 'storage')
 

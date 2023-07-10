4fed5c5d10a6174476e045b923f9c01c3c073380
Haidamac <serge.krinitsa@gmail.com>
2023-07-10 20:34:07 +0300

    add images thumb to ActiveAdmin

--- app/admin/photos.rb
+++ app/admin/photos.rb
@@ -3,6 +3,23 @@ ActiveAdmin.register Photo do
   permit_params :title, :description, :gallery_id, :image
   includes :gallery
   includes image_attachment: :blob
+  
+  index do
+    selectable_column
+    id_column 
+    column :image do |photo|
+      if photo.image.attached?
+        image_tag photo.image.variant(resize: '100x100')
+      else
+        content_tag(:span, "No Image Attached")
+      end
+    end
+    column :title
+    column :description
+    column :gallery
+    actions
+  end
+  
   form do |f|
     f.inputs do
       f.input :gallery
